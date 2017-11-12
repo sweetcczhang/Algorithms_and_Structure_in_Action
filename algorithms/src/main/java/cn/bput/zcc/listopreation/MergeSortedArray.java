b95352dcdf41e75d14bfc992d37ce40a0b5d4b69
bupt_zcc <1215271893@qq.com>
2017-11-12 16:17:25 +0800

    bitoperation

--- algorithms/src/main/java/cn/bput/zcc/listopreation/MergeSortedArray.java
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/MergeSortedArray.java
@@ -4,6 +4,32 @@ package cn.bput.zcc.listopreation;
  * Created by 张城城 on 2017/11/11.
  */
 public class MergeSortedArray {
+
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
+        // write your code here
+        int p = m+n-1;
+        int a = m-1;
+        int b = n-1;
+        while (a>=0 && b>=0){
+            if(A[a]>B[b]){
+                A[p] = A[a];
+                a--;
+            }else {
+                A[p]=B[b];
+                b--;
+            }
+            p--;
+        }
+        while (a>=0){
+            A[p--]=A[a--];
+        }
+        while (b>=0){
+            A[p--] =B[b--];
+        }
+    }
+
+
+
     public static int[] mergeSortedArray(int[]A, int[]B){
         int [] C = new int[A.length+B.length];
         int i=0,j=0,k=0;
