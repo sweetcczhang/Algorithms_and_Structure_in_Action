a5177791bc3751ee9966a462a34f97c706c85758
sweetcczhang <sweetzcc@163.com>
2017-12-04 14:22:28 +0800

    add  search in Rotated sorted array

--- algorithms/src/main/java/cn/bput/zcc/listopreation/Search63.java
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/Search63.java
@@ -3,38 +3,33 @@ package cn.bput.zcc.listopreation;
 /**
  * Created by 张城城 on 2017/12/4.
  */
-public class Search62 {
-    /**
-     *
-     * @param A
-     * @param target
-     * @return
-     */
-    public int search(int[] A, int target) {
+public class Search63 {
+
+    public boolean search(int[] A, int target) {
         // write your code here
         int size = A.length;
         int start = 0;
         int end = A.length-1;
         if(A==null||size==0){
-            return -1;
+            return false;
         }
         if(target>=A[0]){
             while (start<size-1&&A[start]<=A[start+1]){
                 if(target==A[start]){
-                    return start;
+                    return true;
                 }
                 start++;
             }
-            if (A[start]==target) return start;
+            if (A[start]==target) return true;
         }else {
-            while (end>0&&A[end]>A[end-1]){
+            while (end>0&&A[end]>=A[end-1]){
                 if(target==A[end]){
-                    return end;
+                    return true;
                 }
                 end--;
             }
-            if(A[end]==target) return end;
+            if(A[end]==target) return true;
         }
-        return -1;
+        return false;
     }
 }
