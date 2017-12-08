9d6f12a3f91aa1ea9fe00e727509f4aaa669c915
sweetcczhang <sweetzcc@163.com>
2017-12-08 16:41:21 +0800

    add  lintcode 149 max Profit

--- algorithms/src/main/java/cn/bput/zcc/listopreation/MaxProfit149.java
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/MaxProfit149.java
@@ -4,6 +4,13 @@ package cn.bput.zcc.listopreation;
  * Created by 张城城 on 2017/12/8.
  */
 public class MaxProfit149 {
+    /**
+     * 题目描述：lintcode 149
+     * 假设有一个数组，它的第i个元素是一支给定的股票在第i天的价格。
+     * 如果你最多只允许完成一次交易(例如,一次买卖股票),设计一个算法来找出最大利润。
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
         // write your code here
         if(prices==null || prices.length<2) return 0;
@@ -18,7 +25,6 @@ public class MaxProfit149 {
             }
             globalMax = Math.max(globalMax, now);
         }
-
         return globalMax;
     }
 }
