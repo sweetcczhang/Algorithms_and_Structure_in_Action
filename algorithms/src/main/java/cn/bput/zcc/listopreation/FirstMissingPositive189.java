ea92d5136b7e72d3b64e5911a62003a9f1f15fce
sweetcczhang <sweetzcc@163.com>
2017-12-12 09:24:47 +0800

    add  lintcode 189

--- algorithms/src/main/java/cn/bput/zcc/listopreation/FirstMissingPositive189.java
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/FirstMissingPositive189.java
@@ -1,11 +1,15 @@
 package cn.bput.zcc.listopreation;
 
+import cn.bput.zcc.depthFirstSearch.CombinationSum135;
+
 import java.util.Arrays;
 
 /**
  * Created by 张城城 on 2017/12/11.
  */
 public class FirstMissingPositive189 {
+
+    CombinationSum135 combinationSum135;
     /**
      * 题目：丢失的第一个正整数
      * 描述：给出一个无序的整数数组，找出其中没有出现的最小正整数。
@@ -14,10 +18,9 @@ public class FirstMissingPositive189 {
      */
     public int firstMissingPositive(int [] A){
 
-        Arrays.sort(A);
         int flag =-1;
         if(A==null||A.length==0||A[A.length-1]<=0 ) return 1;
-        int[] B = removeDuplicates(A);
+        int[] B = combinationSum135.removeDuplicates(A);
         if(B.length==1 && B[0]!=1) return 1;
         for(int i=0;i<B.length-1;i++){
             if(B[i]>0){
@@ -35,19 +38,5 @@ public class FirstMissingPositive189 {
 
 
         return B[B.length-1]+1;
-    }
-    public int[] removeDuplicates(int[] candidates){
-        Arrays.sort(candidates);
-        int index = 0;
-        for(int i=0; i<candidates.length;i++){
-            if(candidates[i]!=candidates[index]){
-                candidates[++index] = candidates[i];
-            }
-        }
-        int [] num = new int[index+1];
-        for(int i=0;i<=index;i++){
-            num[i] = candidates[i];
-        }
-        return num;
     }
 }
