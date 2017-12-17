94ee084414eaff1025118b5657e1b72b5ae8465d
sweetcczhang <sweetzcc@163.com>
2017-12-17 19:13:12 +0800

    add  lintcode 379 min number

--- algorithms/src/main/java/cn/bput/zcc/listopreation/MinNumber379.java
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/MinNumber379.java
@@ -6,7 +6,42 @@ package cn.bput.zcc.listopreation;
 public class MinNumber379 {
     public String minNumber(int[] nums) {
         // write your code here
-        return null;
+        StringBuilder result = new StringBuilder();
+        if(nums==null || nums.length==0) return "0";
+        quick(nums,0,nums.length-1);
+        for(int i=0;i<nums.length;i++){
+            result.append(String.valueOf(nums[i]));
+        }
+        return result.toString();
+    }
+
+    public void quick(int[] nums, int left, int right){
+        if(left<right){
+            int result = nums[left];
+            int i=left, j=right;
+            while (i<j){
+                while (i<j && compare(nums[j],result)>=0)
+                    j--;
+                while (i<j && compare(result,nums[i])>=0)
+                    i++;
+                if(i<j){
+                    swap(nums,i,j);
+                }
+            }
+            swap(nums, left, i);
+            quick(nums, left,i-1);
+            quick(nums, i+1,right);
+        }
+    }
+    public int compare(int n1, int n2){
+        String s1 = String.valueOf(n1);
+        String s2 = String.valueOf(n2);
+        return (s1+s2).compareTo(s2+s1);
+    }
+    public void swap(int[] nums,int i, int j){
+         int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 }
