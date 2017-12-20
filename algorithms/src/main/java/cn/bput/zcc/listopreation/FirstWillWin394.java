4380f805cabc7a89a3fd69c354331a9f7fd3fdb3
sweetcczhang <sweetzcc@163.com>
2017-12-20 22:49:49 +0800

    add  lintcode 394  first will win

---
+++ algorithms/src/main/java/cn/bput/zcc/listopreation/FirstWillWin394.java
@@ -0,0 +1,13 @@
+package cn.bput.zcc.listopreation;
+
+/**
+ * Created by 张城城 on 2017/12/20.
+ */
+public class FirstWillWin394 {
+    public boolean firstWillWin(int n) {
+        // write your code here
+        if(n%3!=0) return false;
+
+        return true;
+    }
+}
