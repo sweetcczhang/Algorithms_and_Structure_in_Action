2a09dfe945411804b4cb20b67d37d369be022add
sweetcczhang <sweetzcc@163.com>
2018-03-10 15:08:05 +0800

    add add lintcode 392

---
+++ algorithms/src/main/java/cn/bput/zcc/DynamicProgramming/HouseRobber392.java
@@ -0,0 +1,19 @@
+package cn.bput.zcc.DynamicProgramming;
+
+public class HouseRobber392 {
+    public  long houseRobber(int[] A){
+        if(A==null || A.length==0) return 0;
+        if(A.length ==1) return A[0];
+        if(A.length ==2 ) return Math.max(A[0],A[1]);
+        long max0 = 0;
+        long max1 = A[0];
+        long max2 = A[1];
+        for(int i=2;i<A.length;i++){
+            long max3 = A[i] + Math.max(max0,max1);
+            max0 =max1;
+            max1 = max2;
+            max2 = max3;
+        }
+        return Math.max(max1,max2);
+    }
+}
